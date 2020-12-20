db4bfaec8367d45eff8f093984b9f929dfa55131
TuanNguyen2504 <tuannguyentn2504@gmail.com>
2020-12-20 10:55:47 +0700

    Add footer component

--- Front-end/src/components/FooterView/index.js
+++ Front-end/src/components/FooterView/index.js
@@ -1,11 +1,96 @@
-import BrokerCard from 'components/BrokerCard';
+import {
+  FacebookFilled,
+  GooglePlusSquareFilled,
+  LinkedinFilled,
+  PhoneOutlined,
+  TwitterSquareFilled,
+} from '@ant-design/icons';
 import BrokerList from 'containers/BrokerList';
 import React from 'react';
-
+import './index.scss';
+import registeredImg from 'assets/imgs/registered.png';
+import iconPhoneFooter from 'assets/imgs/icon-phone-footer.png';
 function FooterView() {
   return (
-    <div className="container-fluid bg-white ">
+    <div className="container-fluid bg-white footer p-lr-0">
+      {/* Môi giới */}
       <BrokerList />
+      {/* Liên hệ */}
+      <div className="footer-contact p-tb-16">
+        <div className="container d-flex justify-content-between align-i-center">
+          <PhoneOutlined className="phone-icon" />
+          <div className="d-flex flex-direction-column">
+            <h2 className="footer-contact-item">Tư vấn đăng tin</h2>
+            <h2 className="footer-contact-item">
+              <b>0840.67.9081</b>
+            </h2>
+          </div>
+          <div className="d-flex flex-direction-column">
+            <h2 className="footer-contact-item">Tư vấn đào tạo</h2>
+            <h2 className="footer-contact-item">
+              <b>0841.67.9111</b>
+            </h2>
+          </div>
+          <div className="d-flex flex-direction-column">
+            <h2 className="footer-contact-item">Tư vấn quảng cáo</h2>
+            <h2 className="footer-contact-item">
+              <b>0842.67.9022</b>
+            </h2>
+          </div>
+          <div className="d-flex flex-direction-column">
+            <h2 className="footer-contact-item">Hỗ trợ kỹ thuật</h2>
+            <h2 className="footer-contact-item">
+              <b>090.267.9011</b>
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* Thông tin chi tiết */}
+      <div className="container p-tb-32">
+        <p className="t-center" style={{ color: '#888' }}>
+          <span className="font-size-18px">
+            CÔNG TY CỔ PHẦN ĐẦU TƯ TIẾP THỊ BẤT ĐỘNG SẢN VIỆT NAM
+          </span>
+          <br />
+          <strong>Trụ sở:</strong>&nbsp;Số 16 VillaD, The Manor, khu đô thị mới
+          Mỹ Đình Mễ Trì, Phường 1, Quận 12, Thành phố Hồ Chí Minh
+          <br />
+          <strong>Văn phòng:</strong>&nbsp; Tầng 16, Tòa Văn phòng, Vincom, số
+          54 Nguyễn Chí Thanh, Phường 5, Quận 12, Thành phố Hồ Chí Minh
+          <br />
+          <strong>Điện&nbsp;thoại:</strong>&nbsp;0840.67.9081 |{' '}
+          <strong>Email:</strong>&nbsp;batdongsan.vn@gmail.com&nbsp;|{' '}
+          <strong>Website:</strong>&nbsp;<a href="/">Batdongsan.vn</a>
+        </p>
+        <div className="d-flex align-i-center justify-content-center">
+          <img src={registeredImg} width={175} />
+          <div className="d-flex align-i-center m-lr-32">
+            <img src={iconPhoneFooter} />
+            <div className="t-center m-l-16">
+              <h2 style={{ color: '#CE1F26' }}>Hotline</h2>
+              <h2 style={{ color: '#CE1F26' }}>0840.67.9088</h2>
+            </div>
+          </div>
+          <div className="d-flex">
+            <FacebookFilled
+              className="p-lr-4 social-item"
+              style={{ fontSize: 36, color: '#0C86EF' }}
+            />
+            <LinkedinFilled
+              className="p-lr-4 social-item"
+              style={{ fontSize: 36, color: '#0073B1' }}
+            />
+            <GooglePlusSquareFilled
+              className="p-lr-4 social-item"
+              style={{ fontSize: 36, color: '#DB5247' }}
+            />
+            <TwitterSquareFilled
+              className="p-lr-4 social-item"
+              style={{ fontSize: 36, color: '#55ACEF' }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
